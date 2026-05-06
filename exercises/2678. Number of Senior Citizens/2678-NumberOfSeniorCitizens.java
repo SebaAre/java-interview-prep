c61698ad22223194afc5f1a921a7a1e877dc6a35
SebaAre <arellanosebastian5@hotmail.com>
2026-05-06 12:43:43 -0300

    2678: Number Of Senior Citizens Leetcode Exercise

---
+++ exercises/2678. Number of Senior Citizens/2678-NumberOfSeniorCitizens.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+
+        for (int i = 0; i < details.length; i++) {
+            
+            String s = details[i];
+            
+            String ageText = s.substring(11, 13);
+            
+            int age = Integer.parseInt(ageText);
+            
+            if (age > 60) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
